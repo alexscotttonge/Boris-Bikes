4f7a2c556114f52108d7c960ff49aa28bee086fa
Alex <alex@scotttonge.com>
2017-09-27 17:11:37 +0100

    Added tests for release_bike and working? methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,15 @@ require 'docking_station'
 
 describe DockingStation do
   it 'DockingStation instances respond to method release_bike' do
-    expect(subject).to respond_to :release_bike 
+    expect(subject).to respond_to :release_bike
   end
+  it "Creates instance of Bike class when release_bike method is called" do
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+  end
+end
 
+describe Bike do
+  it 'responds true when working? method is called on an instance of Bike' do
+    expect(subject.working?).to be true
+  end
 end
