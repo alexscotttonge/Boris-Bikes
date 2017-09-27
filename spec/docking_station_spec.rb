78588a9c49fc8c038d33efb3ef7284ef96c54e4f
Alex <alex@scotttonge.com>
2017-09-27 17:19:24 +0100

    Moved working? method inside DockingStation test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,10 +7,14 @@ describe DockingStation do
   it "Creates instance of Bike class when release_bike method is called" do
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
-end
-
-describe Bike do
-  it 'responds true when working? method is called on an instance of Bike' do
-    expect(subject.working?).to be true
+  it "responds true when working? method is called on an instance of Bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
+
+# describe Bike do
+#   it 'responds true when working? method is called on an instance of Bike' do
+#     expect(subject.working?).to be true
+#   end
+# end
