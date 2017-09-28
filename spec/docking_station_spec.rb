3bd4917c77748588863ed46f48fdb471a463ecc6
Alex <alex@scotttonge.com>
2017-09-28 12:52:05 +0100

    Created passing unit test to make docking bike possible

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,20 @@
 require 'docking_station'
 
 describe DockingStation do
+  subject(:docking_station) { DockingStation.new }
   it 'DockingStation instances respond to method release_bike' do
-    expect(subject).to respond_to :release_bike
+    expect(docking_station).to respond_to :release_bike
   end
   it "Creates instance of Bike class when release_bike method is called" do
-    expect(subject.release_bike).to be_an_instance_of(Bike)
+    expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
   it "responds true when working? method is called on an instance of Bike" do
-    bike = subject.release_bike
+    bike = docking_station.release_bike
     expect(bike).to be_working
   end
+   it "DockingStation can dock a bike" do
+     expect(docking_station).to respond_to(:dock).with(1).argument
+   end
 end
 
 # describe Bike do
--- user_stories.md
+++ user_stories.md
@@ -1,6 +1,32 @@
 
+## Challenge 2
+
+- As a person,
+- So that I can use a bike,
+- I'd like a docking station to release a bike.
+
+
+- As a person,
+- So that I can use a good bike,
+- I'd like to see if a bike is working
+
 | Objects | Messages |
 | --------|----------|
 | Person  |          |
 | Bike    |     bike_working  |
 | Docking station        |release_bike |
+
+
+
+## Challenge 11
+- As a member of the public
+- So I can decide whether to use the docking station
+- I want to see a bike that has been docked
+
+| Objects | Messages |
+|---------|----------|
+|Person | |
+| Bike | |
+| Docking Station| bike_docked |
+
+DockingStation <-- bike_docked true/false?
